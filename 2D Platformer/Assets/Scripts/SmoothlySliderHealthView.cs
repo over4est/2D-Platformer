1a5c783917271cc6a77bc48eeb9eb1d37ed06fef
over4est <over4est@gmail.com>
2025-03-17 15:33:22 +0300

    Vampirism upd

--- 2D Platformer/Assets/Scripts/SmoothlySliderHealthView.cs
+++ 2D Platformer/Assets/Scripts/SmoothlySliderHealthView.cs
@@ -2,35 +2,42 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(SmoothlyChanger))]
+
 public class SmoothlySliderHealthView : HealthView
 {
+    [SerializeField] private float _changeDuration;
+
+    private Coroutine _corutine;
     private Slider _slider;
-    private SmoothlyChanger _smoothlyChanger;
 
     private void Start()
     {
         _slider = GetComponentInChildren<Slider>();
         _slider.value = MaxValue / MaxValue;
-
-        _smoothlyChanger = GetComponent<SmoothlyChanger>();
-        _smoothlyChanger.ValueChanged += SetSliderValue;
-    }
-
-    private void OnDisable()
-    {
-        _smoothlyChanger.ValueChanged -= SetSliderValue;
     }
 
     protected override void ChangeValue(float value)
     {
-        float startValue = _slider.value * MaxValue;
+        if (_corutine != null)
+            StopCoroutine(_corutine);
 
-        _smoothlyChanger.ChangeValue(startValue, value, MaxValue);
+        _corutine = StartCoroutine(SmoothlyChange(value));
     }
 
-    private void SetSliderValue(float value)
+    private IEnumerator SmoothlyChange(float value)
     {
-        _slider.value = value;
+        float startValue = _slider.value;
+        float targetValue = value / MaxValue;
+        float timeElapsed = 0f;
+
+        while (timeElapsed < _changeDuration)
+        {
+            float delta = timeElapsed / _changeDuration;
+
+            _slider.value = Mathf.Lerp(startValue, targetValue, delta);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
     }
 }
